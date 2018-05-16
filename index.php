a1e88a74d029e33d8f8406a65ec65a7213ccee8e
BryPick <bxp9452@g.rit.edu>
2018-05-16 14:50:49 -0400

    Started on the front end pages

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include_once 'inc/top_layout.php'; ?>
+
+<p>Sed ut perspiciatis unde omnis iste 
+	natus error sit voluptatem accusantium doloremque laudantium, 
+	totam rem aperiam, eaque ipsa quae ab illo inventore veritatis 
+	et quasi architecto beatae vitae dicta sunt explicabo. 
+	Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+	 aut odit aut fugit, sed quia consequuntur 
+	 magni dolores eos qui ratione voluptatem 
+	 sequi nesciunt. Neque porro quisquam est, 
+	 qui dolorem ipsum quia dolor sit amet, 
+	 consectetur, adipisci velit, 
+	 sed quia non numquam eius modi tempora incidunt ut labore 
+	 et dolore magnam aliquam quaerat voluptatem. 
+	 Ut enim ad minima veniam, quis nostrum 
+	 exercitationem ullam corporis suscipit 
+	 laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+	 Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam 
+	 nihil molestiae consequatur, vel 
+	 illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+
+<p>
+	Lorem ipsum dolor sit amet, nostrum reformidans vim at. Ne populo definitionem mea, ad vim aliquip facilis vulputate. Te eos voluptua tincidunt deterruisset, latine iracundia at usu, ad mel maluisset similique adversarium. Sit ut agam sanctus ancillae, simul lucilius neglegentur at has, graeco omnesque dissentiunt cum an. Ei est alii quaeque accusamus. Minim iisque delicata pro no, eum ex odio primis.
+
+Simul omnesque tacimates at vim. Ex dicta dignissim contentiones pri. Nec id suas convenire, qui te eligendi offendit adversarium, copiosae perpetua eu est. Cetero appellantur ne ius, cum ea nulla iudicabit, ne vel essent diceret expetendis. Vix alii comprehensam an. Nusquam tractatos disputationi in vim. Putant offendit euripidis ei pro, vocent dolorum ea has, alia postulant ne vis.
+
+Cu aeque scaevola usu. Reque feugiat torquatos sea ei. Mea zril maiestatis ne, altera epicurei invidunt mea eu. Mei id sint simul, epicuri iudicabit ullamcorper te quo. Brute regione neglegentur et eam.
+
+Mei errem labitur expetenda te. Qui ex homero salutatus, qui eu malis labores, vero affert habemus his ei. Quas maluisset ea qui, ius velit quaestio cu. Et illum verterem accusamus quo, ne ferri simul mandamus nam. Eros altera omnium vis id, probo luptatum ut mel.
+
+Ut pri ocurreret mnesarchum, integre gubergren nec et. Diceret nusquam quo ex. Eos in populo complectitur, sale vituperatoribus et nec. Vis clita vocibus ne, ad pro regione veritus accommodare.
+</p>
+
+<p>
+	Lorem ipsum dolor sit amet, nostrum reformidans vim at. Ne populo definitionem mea, ad vim aliquip facilis vulputate. Te eos voluptua tincidunt deterruisset, latine iracundia at usu, ad mel maluisset similique adversarium. Sit ut agam sanctus ancillae, simul lucilius neglegentur at has, graeco omnesque dissentiunt cum an. Ei est alii quaeque accusamus. Minim iisque delicata pro no, eum ex odio primis.
+
+Simul omnesque tacimates at vim. Ex dicta dignissim contentiones pri. Nec id suas convenire, qui te eligendi offendit adversarium, copiosae perpetua eu est. Cetero appellantur ne ius, cum ea nulla iudicabit, ne vel essent diceret expetendis. Vix alii comprehensam an. Nusquam tractatos disputationi in vim. Putant offendit euripidis ei pro, vocent dolorum ea has, alia postulant ne vis.
+
+Cu aeque scaevola usu. Reque feugiat torquatos sea ei. Mea zril maiestatis ne, altera epicurei invidunt mea eu. Mei id sint simul, epicuri iudicabit ullamcorper te quo. Brute regione neglegentur et eam.
+
+Mei errem labitur expetenda te. Qui ex homero salutatus, qui eu malis labores, vero affert habemus his ei. Quas maluisset ea qui, ius velit quaestio cu. Et illum verterem accusamus quo, ne ferri simul mandamus nam. Eros altera omnium vis id, probo luptatum ut mel.
+
+Ut pri ocurreret mnesarchum, integre gubergren nec et. Diceret nusquam quo ex. Eos in populo complectitur, sale vituperatoribus et nec. Vis clita vocibus ne, ad pro regione veritus accommodare.
+</p>
+
+<p>
+	Lorem ipsum dolor sit amet, nostrum reformidans vim at. Ne populo definitionem mea, ad vim aliquip facilis vulputate. Te eos voluptua tincidunt deterruisset, latine iracundia at usu, ad mel maluisset similique adversarium. Sit ut agam sanctus ancillae, simul lucilius neglegentur at has, graeco omnesque dissentiunt cum an. Ei est alii quaeque accusamus. Minim iisque delicata pro no, eum ex odio primis.
+
+Simul omnesque tacimates at vim. Ex dicta dignissim contentiones pri. Nec id suas convenire, qui te eligendi offendit adversarium, copiosae perpetua eu est. Cetero appellantur ne ius, cum ea nulla iudicabit, ne vel essent diceret expetendis. Vix alii comprehensam an. Nusquam tractatos disputationi in vim. Putant offendit euripidis ei pro, vocent dolorum ea has, alia postulant ne vis.
+
+Cu aeque scaevola usu. Reque feugiat torquatos sea ei. Mea zril maiestatis ne, altera epicurei invidunt mea eu. Mei id sint simul, epicuri iudicabit ullamcorper te quo. Brute regione neglegentur et eam.
+
+Mei errem labitur expetenda te. Qui ex homero salutatus, qui eu malis labores, vero affert habemus his ei. Quas maluisset ea qui, ius velit quaestio cu. Et illum verterem accusamus quo, ne ferri simul mandamus nam. Eros altera omnium vis id, probo luptatum ut mel.
+
+Ut pri ocurreret mnesarchum, integre gubergren nec et. Diceret nusquam quo ex. Eos in populo complectitur, sale vituperatoribus et nec. Vis clita vocibus ne, ad pro regione veritus accommodare.
+</p>
+
+<?php include_once 'inc/bottom_layout.php'; ?>
