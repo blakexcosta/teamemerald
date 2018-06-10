f39632baeef33608eade0d6d1cbd24799cc10d48
BryPick <bxp9452@g.rit.edu>
2018-06-10 13:09:19 -0400

    Created the php database file to connect with MySQL

---
+++ inc/db.class.php
@@ -0,0 +1,28 @@
+<?php 
+	
+	class Database {
+	 
+	    // specify your own database credentials
+	    private $host = "localhost";
+	    private $db_name = "raihn";
+	    private $username = "bxp9452";
+	    private $password = 'citrinebryan';
+	    public $conn;
+	 
+	    // get the database connection
+	    public function getConnection(){
+	 
+	        $this->conn = null;
+	 
+	        try{
+	            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+	            $this->conn->exec("set names utf8");
+	        }catch(PDOException $exception){
+	            error_log("Connection error: " . $exception->getMessage(), 0);
+	        }
+	 
+	        return $this->conn;
+	    }
+	}
+	
+?>
