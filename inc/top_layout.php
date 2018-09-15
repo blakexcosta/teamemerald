33a68588490591f9a0ce0f388252c23e17df9ad4
John Hill <jxh6494@rit.edu>
2018-09-15 17:09:05 -0400

    9_15

--- inc/top_layout.php
+++ inc/top_layout.php
@@ -47,6 +47,9 @@
 				      	<li class="nav-item">
 				        	<a class="nav-link" href="./busdriverroster.php">Bus Driver Roster</a>
 				      	</li>
+								<li class="nav-item">
+									<a class="nav-link" href="./testBlackoutsDRIVERS.php">Test Blackout Drivers</a>
+								</li>
 			      	<?php endif; ?>
 			      	<?php /* if logged in */ if(isset($_SESSION['role']) && ($_SESSION['role'] == "Congregation" || $_SESSION['role'] == "Admin")): ?>
 				      	<li class="nav-item">
