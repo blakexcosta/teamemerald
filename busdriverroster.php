6ff1a946838ca04afea908d552d184fa86c8dd60
Dishank <>
2018-10-23 14:45:44 -0400

    bus diverroster

--- busdriverroster.php
+++ busdriverroster.php
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-<?php 
-    require_once('inc/top_layout.php');
-    require_once('inc/functions.php');
-?>
 
 
-<?php 
-	//Gets the data for all the bus drivers
-	getBusDriverData(); 
-?>
-
-<?php require_once('inc/bottom_layout.php'); ?>
-=======
 <?php
 	session_start();
     require_once("./inc/top_layout.php");
@@ -27,4 +15,3 @@
 ?>
 
 <?php require_once("./inc/bottom_layout.php"); ?>
->>>>>>> JohnGate2
